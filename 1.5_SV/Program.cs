75fb6a08741dd5165fa4e6de6979a3fa447a5286
Даниил <@LAPTOP-EEPRU07S>
2022-06-07 21:22:25 +0700

    Добавьте файлы проекта.

---
+++ 1.5_SV/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Sample5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int n = int.Parse(Console.ReadLine());
+            int a = 1;
+            while (n != 0)
+            {
+                if (n % 10 > 7)
+                {
+                    a = a * (n % 10);
+                }
+                n /= 10;
+            }
+            if (a == 1) a = 0;
+            Console.WriteLine($"Произведение цифр больших семи = {a}");
+        }
+    }
+}
